bae679e1f981c736a4a2439a8d01d712cf8044d1
Dominic Frye <me@itsnebula.net>
2024-12-23 20:50:07 -0500

    feat: add basic express app

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Nocturne API";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs
+            pkgs.bun
+          ];
+        };
+      }
+    );
+}
---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+
+export default app;
